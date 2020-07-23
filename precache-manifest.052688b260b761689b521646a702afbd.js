13d9470f25809a66b92eb7060210e2dca1e4be08
Inga Danek <danekinga@gmail.com>
2020-07-23 19:15:47 +0100

    Updates

---
+++ precache-manifest.052688b260b761689b521646a702afbd.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1b6e25082f47cfb2f596bc71fe3520",
+    "url": "/react-web/index.html"
+  },
+  {
+    "revision": "b59146770ca9e4c82b7e",
+    "url": "/react-web/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "23b669d624760c936695",
+    "url": "/react-web/static/css/main.88f86159.chunk.css"
+  },
+  {
+    "revision": "b59146770ca9e4c82b7e",
+    "url": "/react-web/static/js/2.f1565817.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-web/static/js/2.f1565817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b669d624760c936695",
+    "url": "/react-web/static/js/main.07f2fe66.chunk.js"
+  },
+  {
+    "revision": "6f427c71f1573e7a434e",
+    "url": "/react-web/static/js/runtime-main.6ea6c10d.js"
+  },
+  {
+    "revision": "c455405a82a7b7b6ec9d371d21a17011",
+    "url": "/react-web/static/media/1.c455405a.jpg"
+  },
+  {
+    "revision": "e4ef44f23fb4dd55f1b8e3b5503249bc",
+    "url": "/react-web/static/media/2.e4ef44f2.jpg"
+  },
+  {
+    "revision": "72fad63523058dab80cf23b07f71d26a",
+    "url": "/react-web/static/media/3.72fad635.jpg"
+  },
+  {
+    "revision": "9c21b5c1cb16e1914ff8e4d8d607e527",
+    "url": "/react-web/static/media/4.9c21b5c1.jpg"
+  },
+  {
+    "revision": "da47a199a8a7d61daf4cce95d7ec7aad",
+    "url": "/react-web/static/media/5.da47a199.jpg"
+  },
+  {
+    "revision": "f7c6256851721633d1a0c15c035dc414",
+    "url": "/react-web/static/media/6.f7c62568.jpg"
+  },
+  {
+    "revision": "79de8d299b98d32fe3e71b040cbec53e",
+    "url": "/react-web/static/media/7.79de8d29.jpg"
+  },
+  {
+    "revision": "07946dda876b23b0364d8dd0e031a9d4",
+    "url": "/react-web/static/media/8.07946dda.jpg"
+  },
+  {
+    "revision": "0e728b39f986e9652d08069b6f7c2bec",
+    "url": "/react-web/static/media/Pilecki1.0e728b39.jpg"
+  },
+  {
+    "revision": "78dbf8f73bda66810de0632bbe69dc88",
+    "url": "/react-web/static/media/Pilecki2.78dbf8f7.jpg"
+  },
+  {
+    "revision": "52514103275348faf12d4039ba2b38c1",
+    "url": "/react-web/static/media/Pilecki3.52514103.jpg"
+  },
+  {
+    "revision": "955497829fbc8e81ecc71f50424ef574",
+    "url": "/react-web/static/media/escape1.95549782.jpg"
+  },
+  {
+    "revision": "b15ce1ee74b0191a507faea6f826d944",
+    "url": "/react-web/static/media/escape2.b15ce1ee.jpg"
+  },
+  {
+    "revision": "83fa22ca4a2f062689992bf411d9127a",
+    "url": "/react-web/static/media/escape3.83fa22ca.jpg"
+  },
+  {
+    "revision": "65e466ffe4e17c256eaf7b85e814f8f1",
+    "url": "/react-web/static/media/hideout.65e466ff.jpg"
+  }
+]);
